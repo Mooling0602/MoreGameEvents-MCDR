9cf787885e76d54aa944ca9fb639a45beb6b8cff
木泠 <clemooling@outlook.com>
2024-11-13 10:28:06 +0000

    Fix some errors

--- mg_events/__init__.py
+++ mg_events/__init__.py
@@ -7,6 +7,7 @@ from mg_events.config import check_config, load_config, rawLangPath
 def on_load(server: PluginServerInterface, prev_module):
     load_config(server)
     check_config(server)
+    server.logger.info(rawLangPath)
 
 def on_info(server: PluginServerInterface, info: Info):
     if info.is_from_server:
