b53f0f5afc7ad44ad89b7020ea35ab65ebe5cbee
木泠 <clemooling@outlook.com>
2024-11-13 10:00:19 +0000

    Fix entry load error

--- mg_events/__init__.py
+++ mg_events/__init__.py
@@ -5,8 +5,8 @@ from mcdreforged.api.all import *
 from mg_events.config import check_config, load_config, rawLangPath
 
 def on_load(server: PluginServerInterface, prev_module):
-    load_config()
-    check_config()
+    load_config(server)
+    check_config(server)
 
 def on_info(server: PluginServerInterface, info: Info):
     if info.is_from_server:
