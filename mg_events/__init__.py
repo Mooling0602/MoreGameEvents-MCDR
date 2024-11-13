435877c739b34854785c20c31beee57be6e0856e
木泠 <clemooling@outlook.com>
2024-11-13 13:14:33 +0000

    Fix bug

--- mg_events/__init__.py
+++ mg_events/__init__.py
@@ -1,5 +1,3 @@
-import mg_events.events.death
-
 from mcdreforged.api.all import *
 from mg_events.config import check_config, load_config
 
@@ -9,4 +7,5 @@ def on_load(server: PluginServerInterface, prev_module):
     
 def on_info(server: PluginServerInterface, info: Info):
     if info.is_from_server:
+        import mg_events.events.death
         mg_events.events.death.main(server, info)
